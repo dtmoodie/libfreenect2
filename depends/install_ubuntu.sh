8dcd271aa1235baa484b1d65c7f09b336b2ea1d0
dtmoodie <dtmoodie@gmail.com>
2014-09-17 10:56:15 -0400

    Update install_ubuntu.sh

--- depends/install_ubuntu.sh
+++ depends/install_ubuntu.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-
+sudo apt-get install git build-essentials cmake cmake-curses-gui libXmu-dev libXi-dev libgl-dev dos2unix xorg-dev libglu1-mesa-dev libtool automake libudev-dev libgtk2.0-dev pkg-config libjpeg-turbo8-dev libturbojpeg
 cd `dirname $0`
 DEPENDS_DIR=`pwd`
 
